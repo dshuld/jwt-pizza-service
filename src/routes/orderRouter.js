40972dd029ed6ce17a554f23d4e9154c0e648e24
dshuld <97043453+dshuld@users.noreply.github.com>
2025-03-25 16:42:47 -0600

    remove middleware pt2

--- src/routes/orderRouter.js
+++ src/routes/orderRouter.js
@@ -8,7 +8,6 @@ const orderRouter = express.Router();
 
 const metrics = require('../metrics.js');
 const logger = require('../logger.js');
-app.use(logger.httpLogger);
 
 orderRouter.endpoints = [
   {
