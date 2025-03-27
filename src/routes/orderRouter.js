6877e8753a97c82ba4f9482b73a9ad2f7da4c4f1
dshuld <97043453+dshuld@users.noreply.github.com>
2025-03-27 12:28:10 -0600

    Revert "remove middleware pt2"
    This reverts commit 40972dd029ed6ce17a554f23d4e9154c0e648e24.

--- src/routes/orderRouter.js
+++ src/routes/orderRouter.js
@@ -8,6 +8,7 @@ const orderRouter = express.Router();
 
 const metrics = require('../metrics.js');
 const logger = require('../logger.js');
+app.use(logger.httpLogger);
 
 orderRouter.endpoints = [
   {
